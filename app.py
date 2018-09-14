5700129e3845b87d362596bbdf1cce7fd8c5afe7
Frank <pham.francesco@gmail.com>
2018-09-14 14:49:14 +0200

    first draft of, POST request listening, mysql connection, authentication and configuration loading

--- app.py
+++ app.py
@@ -1,6 +1,58 @@
 from flask import Flask
+from flask import request
+import mysql.connector
+import sys
+import json
+import hashlib
+
+with open('config.json') as config_data_file:
+    config = json.load(config_data_file)
+
+mysqlconfig = config['mysql']
+appconfig = config['app']
+
+mydb = mysql.connector.connect(
+    host=mysqlconfig['host'],
+    port=mysqlconfig['port'],
+    auth_plugin=mysqlconfig['auth_plugin'],
+    user=mysqlconfig['user'],
+    passwd=mysqlconfig['passwd'],
+    database=mysqlconfig['db']
+)
+mycursor = mydb.cursor()
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/electricityusage', methods=['POST'])
+def postusage():
+    reqdata = request.get_json()
+
+    print('request: ', reqdata, file=sys.stderr)
+
+    #check if all values are set
+    if (not 'start' in reqdata) or reqdata['start']=='':
+        return 'start field is required'
+    if (not 'end' in reqdata) or reqdata['end']=='':
+        return 'end field is required'
+    if not 'watthour' in reqdata:
+        return 'watthour field is required'
+    if not 'key' in reqdata:
+        return 'API key is required'
+
+    #authentication check
+    keyhash = (hashlib.sha256(reqdata['key'].encode('utf-8'))).hexdigest()
+    if keyhash != appconfig['API_KEY']:
+        return 'Authentication failed'
+
+    sql = "INSERT INTO consumption (start, end, watthour) VALUES (%s, %s, %s)"
+    entry = (reqdata['start'], reqdata['end'], reqdata['watthour'])
+    mycursor.execute(sql, entry)
+    mydb.commit()
+    return 'ok'
+
+if __name__ == '__main__':
+    app.run(host=appconfig['ip'], port=appconfig['port'], debug=appconfig['debug'])
